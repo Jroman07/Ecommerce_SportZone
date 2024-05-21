b871d1178146be0c97620333e04f353cb8ba71fc
JOSE ROMAN FERNANDEZ <jose.roman.fernandez@est.una.ac.cr>
2024-05-20 20:11:18 -0600

    Update

--- Ecommerce-API/obj/Debug/net7.0/Ecommerce-API.AssemblyInfo.cs
+++ Ecommerce-API/obj/Debug/net7.0/Ecommerce-API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce-API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d5564dc6dd1216c1a433b8b3447478f7a3c542d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce-API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce-API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
